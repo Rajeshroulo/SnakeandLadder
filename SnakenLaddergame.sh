40738e2902005dbe7c0eab1cd8e4bd34e786efe4
Rajeshroulo <rajraval017@gmail.com>
2020-04-17 09:14:42 +0530

    refactored the positions of players

--- SnakenLaddergame.sh
+++ SnakenLaddergame.sh
@@ -2,9 +2,53 @@
 
 read -p "Enter how many players want to play:" n
 
-pos=0
+LADDER=1
+SNAKE=2
+NO_PLAY=0
+POS=0
+INITIAL_POS=0
+GOAL=100
+declare -A posofplayer
 
-die=$((1+RANDOM%6))
-newpos=$(( $pos+$die ))
+function tofindinitialposition( ) {
+ for(( player=1; player<=$n; player++ ))
+   do
+     die=$((1+RANDOM%6))
+     newpos=$(( $POS+$die ))
+     posofplayer[$player]=$newpos
+   done
+}
 
-echo new positon is $newpos
+  function tochecknewpositon( ) {
+   die=$((1+RANDOM%6))
+    check=$((RANDOM%3))
+	case $check in
+		$LADDER )
+			posOfPlayer[$player]=$((${posofPlayer[$player]}+$die))
+                       	if [ ${positionOfPlayer[$player]} -gt $GOAL ]
+                       	then
+                               	posofPlayer[$player]=$((${posofPlayer[$player]}-$die))
+                       	fi
+		;;
+       		$SNAKE )
+               		posofPlayer[$player]=$((${posofPlayer[$player]}-$die))
+       			if [ ${posOfPlayer[$player]} -lt $INITIAL_POS ]
+			then
+				posofPlayer[$player]=$INITIAL_POS
+
+		        fi
+		;;
+		$NO_PLAY )
+                        posofPlayer[$player]=$((${posofPlayer[$player]}+0))
+
+
+    		;;
+        esac
+
+  }
+
+
+
+
+
+
